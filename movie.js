9105269699ed1a470daa6b49b20e3101b8c7e801
Deepak Saini <deepak.saini@servernp.com>
2020-02-21 16:54:12 +0530

    d

---
+++ movie.js
@@ -0,0 +1,33 @@
+class Movie {
+  constructor(title, imageUrl, rate, price, duration, bestseller, description) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.rate = rate;
+    this.price = price;
+    this.duration = duration;
+    this.bestseller = bestseller;
+    this.description = description;
+  }
+}
+
+class MovieList {
+  constructor() {
+
+  }
+}
+
+class MovieItem {
+  constructor() {
+
+  }
+}
+
+
+
+class App {
+  static init() {
+
+  }
+}
+
+App.init()
